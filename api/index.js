061b978804e490fa542dafdea090f3659ae0c421
Lovelie <gaspar2130566@mls.ceu.edu.ph>
2024-12-03 09:01:06 +0800

    arranged cars

--- api/index.js
+++ api/index.js
@@ -5,11 +5,11 @@ app.use(express.json())
 
 const PORT = 4000;
 
-app.get('/tasks', async (req, res) => {
+app.get('/cars', async (req, res) => {
     if (req.query) {
     if (req.query.id) {
-    // http://localhost:4000/tasks?id=1
-    const task = await sql`SELECT * FROM Tasks WHERE Id =
+    // http://localhost:4000/cars?id=1
+    const task = await sql`SELECT * FROM cars WHERE Id =
     ${req.query.id};`;
     if (task.rowCount > 0) {
     res.json(task.rows[0]);
@@ -19,14 +19,14 @@ app.get('/tasks', async (req, res) => {
     return;
     }
     }
-    const tasks = await sql`SELECT * FROM Tasks ORDER BY Id;`;
-    res.json(tasks.rows);
+    const cars = await sql`SELECT * FROM cars ORDER BY Id;`;
+    res.json(cars.rows);
     });
 
-// http://localhost:4000/tasks/1
-app.get('/tasks/:id', async (req, res) => {
+// http://localhost:4000/cars/1
+app.get('/cars/:id', async (req, res) => {
     const id = req.params.id;
-    const task = await sql`SELECT * FROM Tasks WHERE Id =
+    const task = await sql`SELECT * FROM cars WHERE Id =
     ${id};`;
     if (task.rowCount > 0) {
     res.json(task.rows[0]);
@@ -36,25 +36,25 @@ app.get('/tasks/:id', async (req, res) => {
     });
 
 
-// http://localhost:4000/tasks - { "name": "New Task" }
-app.post('/tasks', async (req, res) => {
-    await sql`INSERT INTO Tasks (Name) VALUES
+// http://localhost:4000/cars - { "name": "New Task" }
+app.post('/cars', async (req, res) => {
+    await sql`INSERT INTO cars (Name) VALUES
     (${req.body.name});`;
     res.status(201).json();
     });
 
 
-//http://localhost:4000/tasks/1 - { "name": "Task 1 Updated", "isDone": true } | { "name": "Task 1 Updated" } | { "isDone":  true }
-    app.put('/tasks/:id', async (req, res) => {
+//http://localhost:4000/cars/1 - { "name": "Task 1 Updated", "isDone": true } | { "name": "Task 1 Updated" } | { "isDone":  true }
+    app.put('/cars/:id', async (req, res) => {
     const id = req.params.id;
-    const taskUpdate = await sql`UPDATE Tasks SET Name = ${
+    const taskUpdate = await sql`UPDATE cars SET Name = ${
     (req.body.name != undefined ? req.body.name : task.name)
     }, IsDone = ${
     (req.body.isDone != undefined ? req.body.isDone :
     task.isDone)
     } WHERE Id = ${id};`;
     if (taskUpdate.rowCount > 0) {
-    const task = await sql`SELECT * FROM Tasks WHERE Id =
+    const task = await sql`SELECT * FROM cars WHERE Id =
     ${id};`;
     res.status(200).json(task.rows[0]);
     } else {
@@ -62,10 +62,10 @@ app.post('/tasks', async (req, res) => {
     }
     });
 
-// http://localhost:4000/tasks/1
-app.delete('/tasks/:id', async (req, res) => {
+// http://localhost:4000/cars/1
+app.delete('/cars/:id', async (req, res) => {
 const id = req.params.id;
-const task = await sql`DELETE FROM Tasks WHERE Id = ${id};`;
+const task = await sql`DELETE FROM cars WHERE Id = ${id};`;
 if (task.rowCount > 0) {
 res.status(204).json();
 } else {
