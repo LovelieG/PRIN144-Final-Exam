9b1fd9a523c055f213ec724c4feaae0311b07af5
Lovelie <gaspar2130566@mls.ceu.edu.ph>
2024-12-03 09:31:50 +0800

    added postman try

--- api/index.js
+++ api/index.js
@@ -3,90 +3,105 @@ const { sql } = require('@vercel/postgres');
 const express = require('express')
 const app = express();
 
-const fs = require('fs')
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yaml')
+// const fs = require('fs')
+// const swaggerUi = require('swagger-ui-express');
+// const YAML = require('yaml')
 
-const file  =  fs.readFileSync(process.cwd() + '/swagger.yaml', 'utf8')
-const swaggerDocument = YAML.parse(file)
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+// const file  =  fs.readFileSync(process.cwd() + '/swagger.yaml', 'utf8')
+// const swaggerDocument = YAML.parse(file)
+// const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
-	customCss:
-		'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-	customCssUrl: CSS_URL,
-}));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+// 	customCss:
+// 		'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+// 	customCssUrl: CSS_URL,
+// }));
 
 app.use(express.json())
 
 const PORT = 4000;
 
-app.get('/cars', async (req, res) => {
-    if (req.query) {
-    if (req.query.id) {
-    // http://localhost:4000/cars?id=1
-    const task = await sql`SELECT * FROM cars WHERE Id =
-    ${req.query.id};`;
-    if (task.rowCount > 0) {
-    res.json(task.rows[0]);
-    } else {
-    res.status(404).json();
-    }
-    return;
-    }
-    }
-    const cars = await sql`SELECT * FROM cars ORDER BY Id;`;
-    res.json(cars.rows);
-    });
+const cars = [{ id: 1, plateNumber: 'ABC 123', bodyType: 'Sedan', Color: 'White', firstName: 'Lovelie', lastName: 'Claravall', }];
+let carsID = cars.length;
 
+// app.get('/cars', async (req, res) => {
+//     if (req.query) {
+//         if (req.query.id) {
+//             // http://localhost:4000/cars?id=1
+//             const task = await sql`SELECT * FROM cars WHERE Id =
+//     ${req.query.id};`;
+//             if (task.rowCount > 0) {
+//                 res.json(task.rows[0]);
+//             } else {
+//                 res.status(404).json();
+//             }
+//             return;
+//         }
+//     }
+//     const cars = await sql`SELECT * FROM cars ORDER BY Id;`;
+//     res.json(cars.rows);
+// });
+
+
+app.get('/cars', (req, res) => {
+    if (req.query) {
+        if (req.query.id) {
+            // http://localhost:4000/tasks?id=1
+            const task = cars.find((cars) => task.id === parseInt(req.query.id));
+            if (task) {
+                res.json(task);
+            } else {
+                res.status(404).json();
+            }
+            return;
+        }
+    }
 // http://localhost:4000/cars/1
 app.get('/cars/:id', async (req, res) => {
     const id = req.params.id;
     const task = await sql`SELECT * FROM cars WHERE Id =
     ${id};`;
     if (task.rowCount > 0) {
-    res.json(task.rows[0]);
+        res.json(task.rows[0]);
     } else {
-    res.status(404).json();
+        res.status(404).json();
     }
-    });
+});
 
 
 // http://localhost:4000/cars - { "name": "New Task" }
 app.post('/cars', async (req, res) => {
-    await sql`INSERT INTO cars (Name) VALUES
+    await sql`INSERT INTO cars (id) VALUES
     (${req.body.name});`;
     res.status(201).json();
-    });
+});
 
 
 //http://localhost:4000/cars/1 - { "name": "Task 1 Updated", "isDone": true } | { "name": "Task 1 Updated" } | { "isDone":  true }
-    app.put('/cars/:id', async (req, res) => {
+app.put('/cars/:id', async (req, res) => {
     const id = req.params.id;
-    const taskUpdate = await sql`UPDATE cars SET Name = ${
-    (req.body.name != undefined ? req.body.name : task.name)
-    }, IsDone = ${
-    (req.body.isDone != undefined ? req.body.isDone :
-    task.isDone)
-    } WHERE Id = ${id};`;
+    const taskUpdate = await sql`UPDATE cars SET Name = ${(req.body.name != undefined ? req.body.name : task.name)
+        }, IsDone = ${(req.body.isDone != undefined ? req.body.isDone :
+            task.isDone)
+        } WHERE Id = ${id};`;
     if (taskUpdate.rowCount > 0) {
-    const task = await sql`SELECT * FROM cars WHERE Id =
+        const task = await sql`SELECT * FROM cars WHERE Id =
     ${id};`;
-    res.status(200).json(task.rows[0]);
+        res.status(200).json(task.rows[0]);
     } else {
-    res.status(404).json();
+        res.status(404).json();
     }
-    });
+});
 
 // http://localhost:4000/cars/1
 app.delete('/cars/:id', async (req, res) => {
-const id = req.params.id;
-const task = await sql`DELETE FROM cars WHERE Id = ${id};`;
-if (task.rowCount > 0) {
-res.status(204).json();
-} else {
-res.status(404).json();
-}
+    const id = req.params.id;
+    const task = await sql`DELETE FROM cars WHERE Id = ${id};`;
+    if (task.rowCount > 0) {
+        res.status(204).json();
+    } else {
+        res.status(404).json();
+    }
 });
 
 module.exports = app
